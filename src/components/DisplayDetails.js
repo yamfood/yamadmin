c0bd7a5e821297a5090bcfc4ebbaf3e54f151ff8
Bekhzod1995 <81156beka@gmail.com>
2020-03-04 17:15:39 +0500

    minor change

--- src/components/DisplayDetails.js
+++ src/components/DisplayDetails.js
@@ -3,28 +3,15 @@ import React from 'react';
 const DisplayDetails = (props) => {
   const { dataToDisplay, id } = props;
   if (dataToDisplay[id]) {
-    const formattedData = dataToDisplay[id].map((detail) => {
-      if (detail.label === 'is_blocked') {
-        return (
-          <li key={detail.label}>
-            <b>
-              {detail.label}
-              :
-            </b>
-            {detail.value.toString()}
-          </li>
-        )
-      }
-      return (
-        <li key={detail.label}>
-          <b>
-            {detail.label}
-            :
-          </b>
-          {detail.value}
-        </li>
-      )
-    });
+    const formattedData = dataToDisplay[id].map((detail) => (
+      <li key={detail.label}>
+        <b>
+          {detail.label}
+          :
+        </b>
+        {detail.value.toString()}
+      </li>
+    ));
     return formattedData;
   }
   return null;
