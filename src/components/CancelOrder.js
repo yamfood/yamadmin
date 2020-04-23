c4c8026f91c6f57d42d5f6d5c3ecaaf0311df99e
Igor Kim <igkim90@gmail.com>
2020-04-23 16:06:15 +0500

    added comment modal to cancel order

---
+++ src/components/CancelOrder.js
@@ -0,0 +1,80 @@
+import React, { useState, useImperativeHandle } from 'react';
+import {
+  Modal, Form, Input, Button,
+} from 'antd';
+
+const ModalCancelOrderForm = React.forwardRef(({
+  visible,
+  onCancel,
+  onOk,
+  form,
+}, ref) => {
+  useImperativeHandle(ref, () => ({
+    form,
+  }));
+
+  const { getFieldDecorator } = form;
+
+  return (
+    <Modal
+      visible={visible}
+      title="Комментарий к отмене"
+      okText="Принять"
+      onCancel={onCancel}
+      onOk={onOk}
+    >
+      <Form layout="vertical" itemRef={ref}>
+        <Form.Item label="Причина отказа">
+          {getFieldDecorator('reason')(<Input />)}
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+});
+
+const WrappedModalCancelOrderForm = Form.create({
+  name: 'modalForm',
+})(ModalCancelOrderForm);
+
+const CancelOrder = ({
+  btnType, loading, setVisible, onSubmit,
+}) => {
+  const [isModalVisible, setModalVisibility] = useState(false);
+
+  const showModal = () => {
+    setVisible(false);
+    setModalVisibility(true);
+  };
+
+  const hideModal = () => {
+    setModalVisibility(false);
+  };
+
+  const formRef = React.createRef();
+
+  const handleSubmit = () => {
+    const { form } = formRef.current;
+    form.validateFields((err, values) => {
+      if (err) return;
+      onSubmit(values);
+      form.resetFields();
+      hideModal();
+    });
+  };
+
+  return (
+    <>
+      <Button type={btnType} loading={loading} onClick={showModal}>
+        Отменить
+      </Button>
+      <WrappedModalCancelOrderForm
+        wrappedComponentRef={formRef}
+        visible={isModalVisible}
+        onCancel={hideModal}
+        onOk={handleSubmit}
+      />
+    </>
+  )
+};
+
+export default CancelOrder;
