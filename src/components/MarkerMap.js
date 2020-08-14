c8b6b4c103b0eff55e6c50a3955c52cb44955dfe
kensay98 <rustambabadjanov@mail.ru>
2020-08-15 00:57:34 +0500

    Map changed to yandex

--- src/components/MarkerMap.js
+++ src/components/MarkerMap.js
@@ -1,67 +1,20 @@
 /* eslint-disable */
-import React, {useEffect, useState} from "react";
+import React from "react";
+import { Map, Placemark } from 'react-yandex-maps';
 
 const MarkerMap = (props) => {
-  const {onChange, regions, lat, lng} = props;
-  const [map, setMap] = useState(null);
-  const [marker, setMarker] = useState(null);
-  const gis = require('2gis-maps');
-  useEffect(() => {
-    // No map rendered yet, but the location is present
-    if (!map && lat && lng) {
-      setMap(
-        new gis.Map('map', {
-          container: 'map',
-          center: [lat, lng],
-          zoom: 13
-        })
-      )
-    }
+  const {onChange, lat, lng} = props;
 
-    if (map) {
-      if (!marker) {
-        setMarker(new gis.Marker([lat, lng], {
-          draggable: true,
-          scale: 1.5,
-        }).setLatLng([lat, lng])
-          .on('dragend', function (m) {
-            onChange(this.getLatLng())
-          })
-          .addTo(map))
-      } else {
-        marker.setLatLng([lat, lng])
-        map.flyTo([lat, lng])
-      }
-
-      if(regions){
-        map.on('load', function () {
-          if (regions) {
-            for (let i in regions) {
-              map.addLayer({
-                'id': i,
-                'type': 'fill',
-                'source': {
-                  'type': 'geojson',
-                  'data': regions[i].polygon
-                },
-                'layout': {},
-                'paint': {
-                  'fill-color': '#d97e7e',
-                  'fill-opacity': 0.2
-                }
-              });
-            }
-          }
-        })
-      }
-    }
-
-  }, [lat, lng, map, regions])
-
-  useEffect(() => {
-
-  }, [regions, lat, lng, ]);
-  return <div id='map' style={{width: '100%', height: '100%', ...props.style}}/>
+  return (
+    <Map defaultState={{ center: [lat, lng], zoom: 11 }} width={'100%'} height={300}>
+      <Placemark geometry={[lat, lng]}
+                 options={{ draggable: true }}
+                 onDragEnd={(e) => {
+                     const coords = e.originalEvent.target.geometry._coordinates;
+                     onChange({lat: coords[0], lng: coords[1]})
+                 }}/>
+    </Map>
+  )
 };
 
 export default MarkerMap;
