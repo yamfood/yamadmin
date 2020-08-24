b4cc2fd58945170249a5598d6d0a21af786af25f
kensay98 <rustambabadjanov@mail.ru>
2020-08-24 09:37:55 +0500

    Map and geocoding switched to 2gis

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,6 @@ import Notifications from './Notifications';
 import OrderNew from './OrderNew';
 import api from '../apiRoutes';
 import ClientNew from './ClientNew';
-import {YMaps} from "react-yandex-maps";
 
 
 const openCallsSocket = () => {
@@ -78,48 +77,46 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <YMaps query={{ apikey: '8dbe3f68-3173-479f-ad7c-c05cad82197e' }}>
-      <Layout className="App">
-        <Router history={history}>
-          <QueryParamProvider ReactRouterRoute={Route}>
-            <Notifications />
-            <Navigation />
-            <Switch>
-              <Route path="/login/" component={Login} />
-              <PrivateRoute exact path="/products/create/" component={ProductCreate} />
-              <PrivateRoute exact path="/products" component={Products} />
-              <PrivateRoute exact path="/products/modifiers/:id/edit/" component={ProductModifierEdit} />
-              <PrivateRoute exact path="/products/:id/edit/" component={ProductEdit} />
-              <PrivateRoute exact path="/products/categories/" component={ProductCategories} />
-              <PrivateRoute exact path="/products/categories/create/" component={CategoryCreate} />
-              <PrivateRoute exact path="/products/categories/:id/edit/" component={CategoryEdit} />
-              <PrivateRoute exact path="/kitchens/create/" component={KitchenCreate} />
-              <PrivateRoute exact path="/kitchens" component={KitchensList} />
-              <PrivateRoute exact path="/kitchens/:id/edit/" component={KitchenEdit} />
-              <PrivateRoute exact path="/clients/" component={Clients} />
-              <PrivateRoute exact path="/clients/new/" component={ClientNew} />
-              <PrivateRoute exact path="/clients/:id/" component={TestDetails} />
-              <PrivateRoute exact path="/admins/" component={AdminsList} />
-              <PrivateRoute exact path="/admins/:id/edit/" component={AdminEdit} />
-              <PrivateRoute exact path="/admins/create/" component={AdminCreate} />
-              <PrivateRoute exact path="/riders" component={RidersList} />
-              <PrivateRoute exact path="/riders/:id/edit/" component={RiderEdit} />
-              <PrivateRoute exact path="/orders/active/" component={OrdersActive} />
-              <PrivateRoute exact path="/riders/create/" component={RiderCreate} />
-              <PrivateRoute exact path="/orders/finished/" component={OrdersFinished} />
-              <PrivateRoute exact path="/orders/new/" component={OrderNew} />
-              <PrivateRoute exact path="/orders/:id/" component={OrderDetails} />
-              <PrivateRoute exact path="/orders/:id/logs/" component={OrderLogs} />
-              <PrivateRoute exact path="/announcements/" component={Announcements} />
-              <PrivateRoute exact path="/announcements/create/" component={AnnouncementCreate} />
-              <PrivateRoute exact path="/announcements/:id/edit/" component={AnnouncementEdit} />
-              <PrivateRoute exact path="/params/" component={ParamsList} />
-              <PrivateRoute path="/" component={Home} />
-            </Switch>
-          </QueryParamProvider>
-        </Router>
-      </Layout>
-    </YMaps>
+    <Layout className="App">
+      <Router history={history}>
+        <QueryParamProvider ReactRouterRoute={Route}>
+          <Notifications />
+          <Navigation />
+          <Switch>
+            <Route path="/login/" component={Login} />
+            <PrivateRoute exact path="/products/create/" component={ProductCreate} />
+            <PrivateRoute exact path="/products" component={Products} />
+            <PrivateRoute exact path="/products/modifiers/:id/edit/" component={ProductModifierEdit} />
+            <PrivateRoute exact path="/products/:id/edit/" component={ProductEdit} />
+            <PrivateRoute exact path="/products/categories/" component={ProductCategories} />
+            <PrivateRoute exact path="/products/categories/create/" component={CategoryCreate} />
+            <PrivateRoute exact path="/products/categories/:id/edit/" component={CategoryEdit} />
+            <PrivateRoute exact path="/kitchens/create/" component={KitchenCreate} />
+            <PrivateRoute exact path="/kitchens" component={KitchensList} />
+            <PrivateRoute exact path="/kitchens/:id/edit/" component={KitchenEdit} />
+            <PrivateRoute exact path="/clients/" component={Clients} />
+            <PrivateRoute exact path="/clients/new/" component={ClientNew} />
+            <PrivateRoute exact path="/clients/:id/" component={TestDetails} />
+            <PrivateRoute exact path="/admins/" component={AdminsList} />
+            <PrivateRoute exact path="/admins/:id/edit/" component={AdminEdit} />
+            <PrivateRoute exact path="/admins/create/" component={AdminCreate} />
+            <PrivateRoute exact path="/riders" component={RidersList} />
+            <PrivateRoute exact path="/riders/:id/edit/" component={RiderEdit} />
+            <PrivateRoute exact path="/orders/active/" component={OrdersActive} />
+            <PrivateRoute exact path="/riders/create/" component={RiderCreate} />
+            <PrivateRoute exact path="/orders/finished/" component={OrdersFinished} />
+            <PrivateRoute exact path="/orders/new/" component={OrderNew} />
+            <PrivateRoute exact path="/orders/:id/" component={OrderDetails} />
+            <PrivateRoute exact path="/orders/:id/logs/" component={OrderLogs} />
+            <PrivateRoute exact path="/announcements/" component={Announcements} />
+            <PrivateRoute exact path="/announcements/create/" component={AnnouncementCreate} />
+            <PrivateRoute exact path="/announcements/:id/edit/" component={AnnouncementEdit} />
+            <PrivateRoute exact path="/params/" component={ParamsList} />
+            <PrivateRoute path="/" component={Home} />
+          </Switch>
+        </QueryParamProvider>
+      </Router>
+    </Layout>
   )
 };
 
